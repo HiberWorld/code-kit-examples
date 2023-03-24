23c05584b2e181b0bc9f14ef9bcab9f0e94f1375
Stefan Andersson <stefan.andersson@hiberworld.com>
2023-03-24 12:52:59 +0100

    f

--- .github/actions/open_url/src/index.ts
+++ .github/actions/open_url/src/index.ts
@@ -2,53 +2,12 @@
 /* eslint-disable @typescript-eslint/no-loop-func */
 
 import { exit } from "process";
-import puppeteer from "puppeteer";
 import { exec } from "child_process";
 
 // setTimeout(() => {
 //   console.log("Error: Watchdog triggered.");
 //   exit(1);
 // }, 60000);
-
-const open = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
-
-  page.on("error", (msg) => {
-    console.error("Page crashed:", msg);
-    exit(1);
-  });
-
-  page.on("pageerror", (msg) => {
-    console.error("Uncaught exception in page:", msg);
-    exit(1);
-  });
-
-  let retry = 10;
-
-  while (retry) {
-    const url = "http://localhost:5173/";
-    console.log("Trying " + url);
-    await page
-      .goto(url)
-      .then(() => {
-        retry = 0;
-      })
-      .catch(() => {
-        retry--;
-
-        if (!retry) {
-          exit(1);
-        }
-
-        return new Promise((resolve) => setTimeout(resolve, 3000));
-      });
-  }
-};
-
-console.log("Starting up...");
 
 const promise = new Promise<void>((resolve) => {
   console.log("...");
@@ -104,7 +63,51 @@ const promise2 = new Promise<void>((resolve) => {
 
   console.log("Waiting for listing...");
   await promise2;
+})();
 
+import puppeteer from "puppeteer";
+
+const open = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+
+  page.on("error", (msg) => {
+    console.error("Page crashed:", msg);
+    exit(1);
+  });
+
+  page.on("pageerror", (msg) => {
+    console.error("Uncaught exception in page:", msg);
+    exit(1);
+  });
+
+  let retry = 10;
+
+  while (retry) {
+    const url = "http://localhost:5173/";
+    console.log("Trying " + url);
+    await page
+      .goto(url)
+      .then(() => {
+        retry = 0;
+      })
+      .catch(() => {
+        retry--;
+
+        if (!retry) {
+          exit(1);
+        }
+
+        return new Promise((resolve) => setTimeout(resolve, 3000));
+      });
+  }
+};
+
+console.log("Starting up...");
+
+(async () => {
   console.log("Trying open...");
   await open();
 })();
