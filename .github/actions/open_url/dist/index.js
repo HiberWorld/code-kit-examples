7d3da3978ab1b73ae11abba94cc9673030848538
Stefan Andersson <stefan.andersson@hiberworld.com>
2023-03-24 08:21:13 +0100

    fix asynch

--- .github/actions/open_url/dist/index.js
+++ .github/actions/open_url/dist/index.js
@@ -1,22 +1,21 @@
-"use strict";
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-loop-func */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const process_1 = require("process");
-const puppeteer_1 = __importDefault(require("puppeteer"));
-const browser = await puppeteer_1.default.launch();
+import { exit } from "process";
+import puppeteer from "puppeteer";
+setTimeout(() => {
+    console.log("Error: Watchdog triggered.");
+    exit(1);
+}, 60000);
+const browser = await puppeteer.launch();
 const page = await browser.newPage();
 page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 page.on("error", (msg) => {
     console.error("Page crashed:", msg);
-    (0, process_1.exit)(1);
+    exit(1);
 });
 page.on("pageerror", (msg) => {
     console.error("Uncaught exception in page:", msg);
-    (0, process_1.exit)(1);
+    exit(1);
 });
 let retry = 10;
 while (retry) {
@@ -28,12 +27,8 @@ while (retry) {
         .catch(() => {
         retry--;
         if (!retry) {
-            (0, process_1.exit)(1);
+            exit(1);
         }
         return new Promise((resolve) => setTimeout(resolve, 3000));
     });
 }
-setTimeout(() => {
-    console.log("Error: onRenderDone was not triggered within 60 seconds.");
-    (0, process_1.exit)(1);
-}, 60000);
--- .github/actions/open_url/src/index.ts
+++ .github/actions/open_url/src/index.ts
@@ -9,36 +9,42 @@ setTimeout(() => {
   exit(1);
 }, 60000);
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+const open = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-page.on("error", (msg) => {
-  console.error("Page crashed:", msg);
-  exit(1);
-});
+  page.on("error", (msg) => {
+    console.error("Page crashed:", msg);
+    exit(1);
+  });
 
-page.on("pageerror", (msg) => {
-  console.error("Uncaught exception in page:", msg);
-  exit(1);
-});
+  page.on("pageerror", (msg) => {
+    console.error("Uncaught exception in page:", msg);
+    exit(1);
+  });
 
-let retry = 10;
+  let retry = 10;
 
-while (retry) {
-  await page
-    .goto("http://127.0.0.1:5173/")
-    .then(() => {
-      retry = 0;
-    })
-    .catch(() => {
-      retry--;
+  while (retry) {
+    await page
+      .goto("http://127.0.0.1:5173/")
+      .then(() => {
+        retry = 0;
+      })
+      .catch(() => {
+        retry--;
 
-      if (!retry) {
-        exit(1);
-      }
+        if (!retry) {
+          exit(1);
+        }
 
-      return new Promise((resolve) => setTimeout(resolve, 3000));
-    });
-}
+        return new Promise((resolve) => setTimeout(resolve, 3000));
+      });
+  }
+};
+
+(async () => {
+  await open();
+})();
