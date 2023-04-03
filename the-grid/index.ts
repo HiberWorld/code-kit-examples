ca886cb002189b4742263ff1c93c3304f6a0364c
Stefan Andersson <stefan.andersson@hiberworld.com>
2023-04-03 15:55:44 +0200

    WIP

--- the-grid/index.ts
+++ the-grid/index.ts
@@ -15,9 +15,10 @@ var rsd = new RandomSeed(8);
 
 const grid: HNodeWithMethods[][] = [];
 let unsafeBricks: HNodeWithMethods[] = [];
+let safeBricks: HNodeWithMethods[] = [];
 
 const BRICKSIZE = 4;
-const CENTER = 2;
+const CENTER = 4;
 const MAX = CENTER * 2;
 const GRIDSIZE = MAX + 1;
 
@@ -64,6 +65,7 @@ for (let x = 0; x < GRIDSIZE; x++) {
       grid[x][z] = brick;
 
       if (!safe) unsafeBricks.push(brick);
+      else safeBricks.push(brick);
     }
   }
 }
@@ -71,72 +73,73 @@ for (let x = 0; x < GRIDSIZE; x++) {
 // wire signals
 let i = 1;
 
-for (let x = 0; x < GRIDSIZE; x++) {
-  for (let z = 0; z < GRIDSIZE; z++) {
-    const brick = grid[x][z];
+// for (let x = 0; x < GRIDSIZE; x++) {
+//   for (let z = 0; z < GRIDSIZE; z++) {
 
-    let safe = isSafe(x, z);
+for (i = 0; i < safeBricks.length; i++) {
+  // const brick = grid[x][z];
+  const brick = safeBricks[i];
 
-    if (brick && !safe) {
-      if (unsafeBricks.length) {
-        const otherBrick = rsd.randomFromArray(unsafeBricks);
+  // let safe = isSafe(x, z);
 
-        const id = (i++).toString();
-        brick.signalSource = {
-          id,
-          playerProximitySensor: { maxDistance: 2.8, minDistance: 0 },
-        };
+  // if (brick && !safe) {
+  if (unsafeBricks.length) {
+    const otherBrick = rsd.randomFromArray(unsafeBricks);
 
-        if (brick === otherBrick) {
-          otherBrick.prefabId = "en_m_primitive_window_01";
+    const id = (i++).toString();
+    brick.signalSource = {
+      id,
+      playerProximitySensor: { maxDistance: 2.8, minDistance: 0 },
+    };
 
-          otherBrick.transform!.rot![0] = 90;
-          otherBrick.transform!.pos = [0, -1, 2];
-          otherBrick.transform!.scale = 1;
-        } else {
-        }
-        console.log(
-          "Connecting " +
-            brick.transform?.pos +
-            " with " +
-            otherBrick.transform?.pos
+    if (brick === otherBrick) {
+      otherBrick.prefabId = "en_m_primitive_window_01";
+
+      otherBrick.transform!.rot![0] = 90;
+      otherBrick.transform!.pos = [0, -1, 2];
+      otherBrick.transform!.scale = 1;
+    } else {
+      brick.rendering = { materialID: "palette_01_green" };
+      console.log(
+        "Connecting " +
+          brick.transform?.pos +
+          " with " +
+          otherBrick.transform?.pos
+      );
+
+      otherBrick.signalListener = {
+        target: id,
+        invisibleOnSignal: { dummy: true },
+      };
+
+      let targetPost;
+      targetPost = create({ y: 0 })
+        .animate(
+          {
+            x: [brick.transform?.pos?.[0]!, otherBrick.transform?.pos?.[0]!],
+            z: [brick.transform?.pos?.[2]!, otherBrick.transform?.pos?.[2]!],
+          },
+          { loop: "RESTART", easing: "LINEAR" }
+        )
+        .add(
+          create("sphere_01", {
+            s: 0.1,
+          })
         );
 
-        otherBrick.signalListener = {
-          target: id,
-          invisibleOnSignal: { dummy: true },
-        };
+      targetPost.transform.pos = [0, 1, 0];
 
-        // let targetPost;
-        // targetPost = create({ y: 0 })
-        //   .animate(
-        //     {
-        //       x: [
-        //         brick.transform?.pos?.[0]!,
-        //         otherBrick.transform?.pos?.[0]!,
-        //       ],
-        //       z: [
-        //         brick.transform?.pos?.[2]!,
-        //         otherBrick.transform?.pos?.[2]!,
-        //       ],
-        //     },
-        //     { easing: "LINEAR" }
-        //   )
-        //   .add(
-        //     create("sphere_01", {
-        //       s: 0.1,
-        //     })
-        //   );
+      world.add(targetPost);
 
-        // targetPost.transform.pos = [0, 1, 0];
-
-        // world.add(targetPost);
-
-        unsafeBricks = unsafeBricks.filter((b) => b !== otherBrick);
-      }
+      unsafeBricks = unsafeBricks.filter((b) => b !== otherBrick);
     }
+  } else {
+    console.log("No more unsafe bricks left!");
   }
 }
+
+//}
+//}
 
 renderScene({ root: world, environment: "above_clouds_01" });
 
